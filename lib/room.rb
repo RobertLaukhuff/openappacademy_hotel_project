0a8b4073f5f568ce3e9812370962571b978459cf
Robert Laukhuff <robert@laukcoder.com>
2019-09-20 00:30:08 -0400

    Add Room class functionality

--- lib/room.rb
+++ lib/room.rb
@@ -1,3 +1,34 @@
 class Room
+  def initialize(number)
+    @capacity = number
+    @occupants = []
+  end
 
+  def capacity
+    @capacity
+  end
+  
+  def occupants
+    @occupants
+  end
+
+  def full?
+    if @occupants.length < @capacity
+      return false
+    end
+    true
+  end
+
+  def available_space
+    @capacity - @occupants.length 
+  end
+
+  def add_occupant(guest)
+    if self.full?
+      return false
+    end
+
+    @occupants << guest
+    true
+  end
 end
