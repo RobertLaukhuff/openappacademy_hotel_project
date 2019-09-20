63f632aecac8bd33cc58b278abc1c8c95385df26
Robert Laukhuff <robert@laukcoder.com>
2019-09-20 12:43:11 -0400

    Build Hotel Class functionality

--- lib/hotel.rb
+++ lib/hotel.rb
@@ -1,5 +1,54 @@
 require_relative "room"
+require "byebug"
 
 class Hotel
-  
+  def initialize(name, rooms)
+    @name = name
+    @rooms = Hash.new
+    rooms.each do |k, v|
+      @rooms[k] = Room.new(v)
+    end
+  end
+
+  def name
+    name = @name.split(' ')
+    name.map! {|n| n.capitalize!}
+    name.join(' ')
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def room_exists?(room_name)
+    @rooms.has_key?(room_name)
+  end
+
+  def check_in(guest, room_name)
+    if room_exists?(room_name)
+      if @rooms[room_name].add_occupant(guest)
+        print "check in successful"
+      else
+        print "sorry, room is full"
+      end
+    else
+      print "sorry, room does not exist"
+    end
+  end
+
+  def has_vacancy?
+    @rooms.each_value do |room|
+      if !room.full?
+        return true
+      end
+    end
+
+    false
+  end
+
+  def list_rooms
+    @rooms.each do |room_name, room|
+      print room_name + " " +room.available_space.to_s + "\n"
+    end
+  end
 end
